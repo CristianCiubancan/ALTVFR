f61c030b06d62630e6505921ce20ad31ef0041fb
CristianCiubancan <balooprogames@gmail.com>
2025-04-14 22:12:09 +0300

    fix

---
+++ src/core/plugin-manager/types.ts
@@ -0,0 +1,13 @@
+/**
+ * Re-export plugin types from the plugin system interfaces
+ */
+export {
+  PluginManifest,
+  PluginLifecycle,
+  ClientPlugin,
+  ServerPlugin,
+  PluginStatus,
+  PluginRegistryEntry,
+  PluginRegistry,
+  PluginManager
+} from '../plugin-system/interfaces';
